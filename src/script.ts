dfd2a09b7b7b63e757766a061d64a01a05e0f592
sernamar <sernamar@protonmail.com>
2022-03-02 00:43:08 +0100

    Use SQLite to store the links

---
+++ src/script.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Fullstack tutorial for GraphQL",
+      url: "www.howtographql.com",
+    },
+  });
+
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((error) => {
+    throw error;
+  })
+  .finally(async () => await prisma.$disconnect());
